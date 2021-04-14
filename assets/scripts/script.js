97e02aeb34cf86ecee44c1522618795775b7bad3
Levi Kane <oneleggedlevi09@gmail.com>
2021-04-13 19:35:18 -0500

    added a cancel button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,3 +7,29 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.datepicker').datepicker()
 })
+
+var addItemBtn = $('#addItemBtn')
+addItemBtn.on('click', function () {
+  $('#modalForm').trigger('reset')
+  $('#itemSubmit').removeClass('modal-close')
+})
+
+var itemSubmit = $('#itemSubmit')
+itemSubmit.on('click', function () {
+  var itemDate = $('#itemDate').val()
+  var itemDescription = $('#itemDescription').val()
+  var expenseTypeSelection = $('#expenseTypeSelection').val()
+  var itemAmount = parseFloat($('#itemAmount').val())
+  if (
+    itemDate != '' &&
+    expenseTypeSelection !== null &&
+    itemDescription != '' &&
+    itemAmount != ''
+  ) {
+    itemSubmit.addClass('modal-close')
+  }
+  console.log(itemDate)
+  console.log(expenseTypeSelection)
+  console.log(itemDescription)
+  console.log(itemAmount)
+})
